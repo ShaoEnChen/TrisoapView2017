b7d07ea2a422990bb28b5b8202e5817b81953d3f
ShaoEnChen <seanchen47@gmail.com>
2017-10-07 02:41:37 +0800

    finish content - index

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ include_once('header.html');
 include_once('nav.html');
 if(!isset($_GET['route']))
 	include_once('jumbotron.html');
-include_once('content.html');
+include_once('content/index.html');
 include_once('footer.html');
