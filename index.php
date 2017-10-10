57def92f64704b7cebae304888f1d7206528588b
ShaoEnChen <seanchen47@gmail.com>
2017-10-10 17:41:40 +0800

    fix nav, finish img jumbotron, working on about.php page

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 
 include_once('header.html');
 include_once('nav.html');
-if(!isset($_GET['route']))
+if(!isset($_GET['route'])) {	// index
+	include_once('jumbotron_index.html');
+	include_once('content/index.html');
+}
+else {
 	include_once('jumbotron.html');
-include_once('content/index.html');
+	include_once('content/' . $_GET['route'] . '.html');
+}
 include_once('footer.html');
